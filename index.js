44f86b74abebef32f15f1450728bfe97d8eb60be
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-19 11:38:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride<= 400) {
+    return "This one is on me!"
+  } else if (ride<2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if(ride>2500) {
+    return "No can do."
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  let  message = (city==="NYC")?"Ok, sounds good.":'No go.';
+  //  message = (city!="NYC")?'No go.':"Ok, sounds good.";
 
-function switchOnCharmFromTip(){
+  return message;
+}
+console.log(ternaryCheckCity("NYCuh"))
+
+
+let tip = "generous";
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ 
+  switch(tip) {
+  case "generous" :
+    return "Thank you so much."
+  case "not as generous" :
+    return "Thank you."  
+   default :
+    return "Bye." 
+  }
 }
